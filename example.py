130e9b9319e4101006b00564ad0b8f8a289d1278
Ben Abbatematteo <benabba95@gmail.com>
2020-07-26 12:15:52 -0400

    pybullet install and minimal example with collision

---
+++ example.py
@@ -0,0 +1,25 @@
+import pybullet as p
+import pybullet_data
+
+import numpy as np
+
+p.connect(p.GUI) # alternatively p.connect(p.DIRECT) if you don't want GUI
+p.setGravity(0,0,-10)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+planeUid=p.loadURDF("plane.urdf", [0, 0, 0])
+
+# %% create the robot
+id_ball1 = p.loadURDF('ball.urdf',
+                      basePosition=[0,0,0],
+                      baseOrientation=[0,0,0,1])
+
+id_ball2 = p.loadURDF('ball.urdf',
+                      basePosition=[0,0,0],
+                      baseOrientation=[0,0,0,1])
+
+while True:
+    contactPoints = p.getContactPoints(id_ball1, id_ball2)
+    if len(contactPoints) > 0:
+        print(len(contactPoints))
+        
+    p.stepSimulation()
---
+++ pybullet-install.sh
@@ -0,0 +1,10 @@
+
+# assuming ``python`` is ROS python2
+mkdir -p ~/envs
+cd ~/envs
+virtualenv -p python pbcollision --system-site-packages
+
+source ~/envs/pbcollision/bin/activate
+pip install numpy
+pip install pybullet
+# pip install open3d
